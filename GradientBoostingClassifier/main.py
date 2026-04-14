59af02e87a4cbf3d6e1283cdf02df842a6c431be
Nhi <nhipnu9507@ut.edu.vn>
2026-04-14 14:32:37 +0700

    update gradient boosting skeleton + project structure

--- GradientBoostingClassifier/main.py
+++ GradientBoostingClassifier/main.py
@@ -1,13 +1,12 @@
-from models.GradientBoosting import GradientBoostingClassifier
-from metrics.Accuracy import accuracy_score
-from metrics.ClassificationReport import classification_report
-from metrics.ConfusionMatrix import confusion_matrix
-from visualization.ConfusionMatrixDisplay import ConfusionMatrixDisplay
-
+from models.gradient_boosting import gradient_boosting_classifier
+from metrics.accuracy import accuracy_score
+from metrics.classification_report import classification_report
+from metrics.confusion_matrix import confusion_matrix
+from visualization.confusion_matrix_display import confusion_matrix_display
 X = [[1], [2], [3], [4]]
 y = [0, 1, 0, 1]
 
-model = GradientBoostingClassifier()
+model = gradient_boosting_classifier()
 
 model.fit(X, y)
 
@@ -17,7 +16,7 @@ acc = accuracy_score(y, y_pred)
 report = classification_report(y, y_pred)
 cm = confusion_matrix(y, y_pred)
 
-disp = ConfusionMatrixDisplay(cm)
+disp = confusion_matrix_display(cm)
 
 print("Accuracy:", acc)
 print("Report:", report)
